a7246d538ff1a6d7f83ebf51d5b92a219ddd8fd1
Kyle Cermak <38162852+Cermak059@users.noreply.github.com>
2021-04-22 12:10:22 -0500

    Center Decline/Approval Requests

--- BowlNow/Network/GlobalRequests.swift
+++ BowlNow/Network/GlobalRequests.swift
@@ -59,6 +59,7 @@ class GlobalRequests {
             }
         }.resume()
     }
+    
     func ActiveCentersList(completion: @escaping completion)  {
     
         guard let url = URL(string: "https://openbowlservice.com/api/v1/center/registration") else {
@@ -153,6 +154,121 @@ class GlobalRequests {
                     }
                     else {
                         DispatchQueue.main.async {
+                            completion(false, "Oh no! Something went wrong on our end... Please try again.", [])
+                        }
+                    }
+            }
+        }.resume()
+    }
+    
+    func ApproveCenter(AuthToken: String, Moid: String, Url: String, completion: @escaping completion) {
+        guard let url =  URL(string: "https://openbowlservice.com/api/v1/center/pending") else {
+            completion(false, "Bad Url", [])
+            return
+        }
+        
+        let body: [String: String] = ["Moid": Moid]
+        
+        guard let finalbody = try? JSONSerialization.data(withJSONObject: body) else {
+            completion(false, "Json body is corrupt...Please try again!", [])
+            return
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.httpBody = finalbody
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.setValue(AuthToken, forHTTPHeaderField: "X-Auth-Token")
+              
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+                  
+            if let httpResponse = response as? HTTPURLResponse{
+                if httpResponse.statusCode == 200{
+                    guard let data = data else {return}
+                    guard let finalData = try? JSONDecoder().decode(ApiResponse.self, from: data) else {
+                        DispatchQueue.main.async {
+                            completion(false, "Json response is corrupt... Please try again!", [])
+                        }
+                        return
+                    }
+                    DispatchQueue.main.async {
+                        completion(true, finalData.Results, [])
+                    }
+                    return
+                }
+                    else if httpResponse.statusCode == 400{
+                        guard let data = data else {return}
+                        guard let finalData = try? JSONDecoder().decode(ApiResponse.self, from: data) else {
+                            DispatchQueue.main.async {
+                                completion(false, "Json response is corrupt... Please try again!", [])
+                            }
+                            return
+                        }
+                        DispatchQueue.main.async {
+                            completion(true, finalData.Results, [])
+                        }
+                        return
+                    }
+                    else {
+                        DispatchQueue.main.async {
+                            completion(false, "Oh no! Something went wrong on our end... Please try again.", [])
+                        }
+                    }
+            }
+        }.resume()
+    }
+    
+    func DeclineCenter(AuthToken: String, Moid: String, completion: @escaping completion) {
+        guard let url =  URL(string: "https://openbowlservice.com/api/v1/center/pending") else {
+            completion(false, "Bad Url", [])
+            return
+        }
+        
+        let body: [String: String] = ["Moid": Moid]
+        
+        guard let finalbody = try? JSONSerialization.data(withJSONObject: body) else {
+            completion(false, "Json body is corrupt...Please try again!", [])
+            return
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "DELETE"
+        request.httpBody = finalbody
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.setValue(AuthToken, forHTTPHeaderField: "X-Auth-Token")
+              
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+                  
+            if let httpResponse = response as? HTTPURLResponse{
+                if httpResponse.statusCode == 200{
+                    guard let data = data else {return}
+                    guard let finalData = try? JSONDecoder().decode(ApiResponse.self, from: data) else {
+                        DispatchQueue.main.async {
+                            completion(false, "Json response is corrupt... Please try again!", [])
+                        }
+                        return
+                    }
+                    DispatchQueue.main.async {
+                        completion(true, finalData.Results, [])
+                    }
+                    return
+                }
+                    else if httpResponse.statusCode == 400{
+                        guard let data = data else {return}
+                        guard let finalData = try? JSONDecoder().decode(ApiResponse.self, from: data) else {
+                            DispatchQueue.main.async {
+                                completion(false, "Json response is corrupt... Please try again!", [])
+                            }
+                            return
+                        }
+                        DispatchQueue.main.async {
+                            completion(true, finalData.Results, [])
+                        }
+                        return
+                    }
+                    else {
+                        DispatchQueue.main.async {
+                            print(httpResponse.statusCode)
                             completion(false, "Oh no! Something went wrong on our end... Please try again.", [])
                         }
                     }
