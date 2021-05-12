eecf60664bd778bfa5238f0ff25b987626f3ec20
Kyle Cermak <38162852+Cermak059@users.noreply.github.com>
2021-05-11 20:41:14 -0500

    UI updates

--- BowlNow/UserViews/SignUp.swift
+++ BowlNow/UserViews/SignUp.swift
@@ -22,20 +22,34 @@ struct SignUp: View {
                         PlatformInfo()
                         VStack {
                             VStack {
-                                NavigationLink(destination: UserRegister(), isActive: $isShowingUser) { EmptyView() }
+                                Spacer()
+                                NavigationLink(destination: UserRegister(), isActive: $isShowingUser) { EmptyView()}
                                 UserButton(isShowingUser: $isShowingUser)
                                     .frame(maxWidth: .infinity, maxHeight: geometry.size.height/7, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
                                     .background(Color(red: 146/255, green: 107/255, blue: 214/255, opacity: 1.0))
                                     .cornerRadius(10)
-                                    .padding([.horizontal, .bottom])
+                                    .padding(.horizontal)
+                                HStack {
+                                    Divider()
+                                        .frame(maxWidth:.infinity, maxHeight:2)
+                                        .background(Color(red: 146/255, green: 107/255, blue: 214/255, opacity: 1.0))
+                                        .padding()
+                                    Text("Or")
+                                        .font(.subheadline)
+                                        .foregroundColor(.black)
+                                    Divider()
+                                        .frame(maxWidth:.infinity, maxHeight:2)
+                                        .background(Color(red: 146/255, green: 107/255, blue: 214/255, opacity: 1.0))
+                                        .padding()
+                                }
                                 NavigationLink(destination: CenterRegister(), isActive: $isShowingCenter) { EmptyView() }
                                 CenterButton(isShowingCenter: $isShowingCenter)
                                     .frame(maxWidth: .infinity, maxHeight: geometry.size.height/7, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                                    .background((Color(red: 131/255, green: 202/255, blue: 238/255, opacity: 1.0)))
+                                    .background(Color(red: 131/255, green: 202/255, blue: 238/255, opacity: 1.0))
                                     .cornerRadius(10)
                                     .padding([.horizontal, .bottom])
+                                Spacer()
                             }.padding()
-                            Spacer()
                             SwipeDown()
                         }.navigationBarTitle("",displayMode: .inline).navigationBarHidden(true)
                     }.background(Image("retro_background")
@@ -55,7 +69,8 @@ struct PlatformInfo: View {
             HStack {
                 Text("Welcome")
                     .font(.largeTitle)
-                    .bold().frame(maxWidth:.infinity, alignment: .leading)
+                    .bold()
+                    .frame(maxWidth:.infinity, alignment: .leading)
                     .padding()
                     .fixedSize(horizontal: false, vertical: true)
                     .foregroundColor(Color(red: 146/255, green: 107/255, blue: 214/255, opacity: 1.0))
@@ -66,12 +81,12 @@ struct PlatformInfo: View {
                     .frame(maxWidth: 50)
                     .padding()
             }
-            Text("Our platform creates an opportunity for the bowling community to earn and redeem rewards at their favorite centers. Made for bowlers, by bowlers, our mission is to provide a platform of inclusivity, support and feedback! ")
+            Text("Our platform creates an opportunity for the bowling community to earn and redeem rewards at their favorite centers. Made for bowlers, by bowlers, our mission is to provide a custom platform of inclusivity, support and feedback!")
                 .frame(maxWidth:.infinity, alignment: .leading)
                 .padding([.horizontal,.bottom])
                 .font(.subheadline)
                 .foregroundColor(.black)
-            Text("Please start by creating an account as a regular user or a bowling center below: ")
+            Text("Please start by creating an account below: ")
                 .frame(maxWidth:.infinity, alignment: .leading)
                 .padding([.horizontal,.bottom])
                 .font(.subheadline)
