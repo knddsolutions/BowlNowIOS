f86c699133f417cd972f1094463bb91e506a8cdd
Kyle Cermak <38162852+Cermak059@users.noreply.github.com>
2021-05-11 20:55:12 -0500

    User defaults changes

--- BowlNow/UserViews/MyCenters.swift
+++ BowlNow/UserViews/MyCenters.swift
@@ -10,22 +10,27 @@ import UIKit
 import Kingfisher
 
 struct MyCenters: View {
-    var myCenters: [String] = UserDefaults.standard.stringArray(forKey: "MyCenters") ?? []
+    var myCenters: [String] {
+        UserDefaults.standard.stringArray(forKey: "MyCenters") ?? []
+    }
     var centerUserRequests = CenterUserRequests()
     @Binding var ActiveCenters: [CenterObject]
     @State var isHome: Bool = false
+    @Binding var rootIsActive: Bool
     var body: some View {
         GeometryReader { geometry in
             ZStack {
                 Color.white.edgesIgnoringSafeArea(.all)
                 VStack{
-                    NavigationLink(destination: Home(), isActive: $isHome) { EmptyView() }
+                    NavigationLink(destination: TabbedView(viewRouter: ViewRouter(), rootIsActive: $rootIsActive), isActive: $isHome) { EmptyView() }
                     ScrollView {
                         ForEach(ActiveCenters, id: \.Moid) { center in
                             if myCenters.contains(center.Moid) {
                                 let url = URL(string: center.BannerURL!)
                                 Button(action: {
                                     UserDefaults.standard.set(center.BannerURL, forKey: "BannerURL")
+                                    UserDefaults.standard.set(center.Center, forKey: "CenterName")
+                                    UserDefaults.standard.set(center.Moid, forKey: "CenterMoid")
                                     self.isHome.toggle()
                                 }) {
                                     KFImage(url)
@@ -48,13 +53,14 @@ struct MyCenters: View {
                                         .resizable()
                                         .scaledToFit()
                                 }
+                                Divider()
                             }
                         }
                         NavigationLink(
-                            destination: CenterList(ActiveCenters: $ActiveCenters),
+                            destination: CenterList(rootIsActive: $rootIsActive, ActiveCenters: $ActiveCenters),
                             label: {
                                 Text("+ ADD CENTER")
-                                    .frame(minWidth: 0, maxWidth: .infinity, minHeight:75, maxHeight: 75)
+                                    .frame(minWidth: 0, maxWidth: .infinity, minHeight:50, maxHeight: 50)
                                     .font(.system(size: 18))
                                     .padding()
                                     .foregroundColor(Color(red: 146/255, green: 107/255, blue: 214/255, opacity: 1.0))
@@ -62,15 +68,13 @@ struct MyCenters: View {
                                         RoundedRectangle(cornerRadius: 10)
                                             .stroke(Color(red: 146/255, green: 107/255, blue: 214/255, opacity: 1.0), style: StrokeStyle(lineWidth: 4.0, lineCap: .round, dash: [20, 10])))
                             }).padding()
-                        Spacer()
                     }
                 }.background(Image("retro_background").resizable()
                                 .aspectRatio(geometry.size, contentMode: .fill)
-                                .edgesIgnoringSafeArea(.all).opacity(0.1)).navigationBarTitle("My Centers", displayMode: .inline)
+                                .edgesIgnoringSafeArea(.all).opacity(0.1))
+                .navigationBarTitle("My Centers", displayMode: .inline)
             }
         }
     }
 }
-
-//TODO REQUEST FOR CURRENT CENTER MEMBERSHIPS
 
