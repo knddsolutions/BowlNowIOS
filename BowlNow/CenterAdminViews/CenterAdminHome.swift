d9e49a89e797ae0cf6c2b2e737d30802636a22ff
Kyle Cermak <38162852+Cermak059@users.noreply.github.com>
2021-05-11 20:35:12 -0500

    create admin home

---
+++ BowlNow/CenterAdminViews/CenterAdminHome.swift
@@ -0,0 +1,20 @@
+//
+//  CenterAdminHome.swift
+//  BowlNow
+//
+//  Created by Kyle Cermak on 4/16/21.
+//
+
+import SwiftUI
+
+struct CenterAdminHome: View {
+    var body: some View {
+        Text("Center Admin Home!")
+    }
+}
+
+struct CenterAdminHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CenterAdminHome()
+    }
+}
