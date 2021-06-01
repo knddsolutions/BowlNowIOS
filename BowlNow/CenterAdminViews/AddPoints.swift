f19a34699b3a8ab92805575b305dbd4ba3276a63
Kyle Cermak <38162852+Cermak059@users.noreply.github.com>
2021-06-01 18:30:39 -0500

    Admin add/redeem point views

---
+++ BowlNow/CenterAdminViews/AddPoints.swift
@@ -0,0 +1,16 @@
+//
+//  AddPoints.swift
+//  BowlNow
+//
+//  Created by Kyle Cermak on 6/1/21.
+//
+
+import SwiftUI
+
+struct AddPoints: View {
+    @Binding var AccountName: String
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
---
+++ BowlNow/CenterAdminViews/RedeemPoints.swift
@@ -0,0 +1,16 @@
+//
+//  RedeemPoints.swift
+//  BowlNow
+//
+//  Created by Kyle Cermak on 6/1/21.
+//
+
+import SwiftUI
+
+struct RedeemPoints: View {
+    @Binding var AccountEmail: String
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
