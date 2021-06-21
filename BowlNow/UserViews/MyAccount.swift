14eeaf9f054b90d3555f5736f3f367b1684f1e1a
Kyle Cermak <38162852+Cermak059@users.noreply.github.com>
2021-06-21 10:29:03 -0500

    Working concept 1.1

--- BowlNow/UserViews/MyAccount.swift
+++ BowlNow/UserViews/MyAccount.swift
@@ -16,7 +16,7 @@ struct MyAccount: View {
     @State var Email: String = UserDefaults.standard.string(forKey: "storeEmail") ?? ""
     @State var BirthDate: String = UserDefaults.standard.string(forKey: "Birthday") ?? ""
     @State var FirstInitial: String = ""
-    @Binding var rootIsActive:Bool
+    @Binding var rootIsActive: Bool
     @Environment(\.presentationMode) var presentationMode
     var body: some View {
         GeometryReader { geometry in
@@ -33,7 +33,7 @@ struct MyAccount: View {
                                         .font(.largeTitle)
                                         .bold())
                         Text("\(Fname) \(Lname)")
-                            .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                            .font(.title)
                         Spacer()
                         HStack {
                             HStack {
@@ -55,68 +55,66 @@ struct MyAccount: View {
                             }
                         }.frame(width:geometry.size.width)
                     }.frame(width: geometry.size.width, height:geometry.size.height/2)
-                    VStack(alignment: .leading) {
-                        HStack {
-                            Text(" User ID: ")
-                                .font(.subheadline)
-                                .bold()
-                            + Text("\(CenterUserID)")
-                                .font(.subheadline)
-                        }.padding([.leading, .top])
-                        Divider()
-                        HStack {
-                            Text(" First Name: ")
-                                .font(.subheadline)
-                                .bold()
-                            + Text("\(Fname)")
-                                .font(.subheadline)
-                        }.padding([.horizontal])
-                        HStack {
-                            Text(" Last Name: ")
-                                .font(.subheadline)
-                                .bold()
-                            + Text("\(Lname)")
-                                .font(.subheadline)
-                        }.padding([.top, .horizontal])
-                        HStack {
-                            Text(" Email: ")
-                                .font(.subheadline)
-                                .bold()
-                            + Text("\(Email)")
-                                .font(.subheadline)
-                        }.padding([.top, .horizontal])
-                        HStack {
-                            Text(" Birthdate: ")
-                                .font(.subheadline)
-                                .bold()
-                            + Text("\(BirthDate)")
-                                .font(.subheadline)
-                        }.padding()
-                        Button(action: {
-                            self.presentationMode.wrappedValue.dismiss()
-                        }){
-                            Text("Change My Center")
-                                .foregroundColor(.white)
-                                .bold()
-                        }.frame(maxWidth: .infinity, minHeight: 50, maxHeight: 50).background(Color(red: 131/255, green: 202/255, blue: 238/255, opacity: 1.0))
-                        .cornerRadius(10)
-                        .padding([.horizontal])
-                        .padding(.bottom, 5)
-                        Button(action: {
-                            rootIsActive.toggle()
-                        }){
-                            Text("Log Out")
-                                .foregroundColor(.white)
-                                .bold()
-                        }.frame(maxWidth: .infinity, minHeight: 50, maxHeight: 50).background(Color(red: 146/255, green: 107/255, blue: 214/255, opacity: 1.0))
-                        .cornerRadius(10)
-                        .padding(.horizontal)
-                        Spacer()
-                    }.frame(maxWidth: .infinity, maxHeight: .infinity).background(Color(.white))
-                }.background(Image("retro_background")
-                                .resizable()
-                                .aspectRatio(geometry.size, contentMode: .fill)
-                                .edgesIgnoringSafeArea(.all).opacity(0.1))
+                    ScrollView {
+                        VStack(alignment: .leading, spacing: 15) {
+                            HStack {
+                                Text(" User ID: ")
+                                    .font(.subheadline)
+                                    .bold()
+                                + Text("\(CenterUserID)")
+                                    .font(.subheadline)
+                            }.padding([.leading, .top])
+                            Divider()
+                            HStack {
+                                Text(" First Name: ")
+                                    .font(.subheadline)
+                                    .bold()
+                                + Text("\(Fname)")
+                                    .font(.subheadline)
+                            }.padding([.horizontal])
+                            HStack {
+                                Text(" Last Name: ")
+                                    .font(.subheadline)
+                                    .bold()
+                                + Text("\(Lname)")
+                                    .font(.subheadline)
+                            }.padding([.top, .horizontal])
+                            HStack {
+                                Text(" Email: ")
+                                    .font(.subheadline)
+                                    .bold()
+                                + Text("\(Email)")
+                                    .font(.subheadline)
+                            }.padding([.top, .horizontal])
+                            HStack {
+                                Text(" Birthdate: ")
+                                    .font(.subheadline)
+                                    .bold()
+                                + Text("\(BirthDate)")
+                                    .font(.subheadline)
+                            }.padding()
+                            Button(action: {
+                                self.presentationMode.wrappedValue.dismiss()
+                            }){
+                                Text("Change My Center")
+                                    .foregroundColor(.white)
+                                    .bold()
+                            }.frame(maxWidth: .infinity, minHeight: 50, maxHeight: 50).background(Color(red: 131/255, green: 202/255, blue: 238/255, opacity: 1.0))
+                            .cornerRadius(10)
+                            .padding([.horizontal])
+                            .padding(.bottom, 5)
+                            Button(action: {
+                                rootIsActive.toggle()
+                            }){
+                                Text("Log Out")
+                                    .foregroundColor(.white)
+                                    .bold()
+                            }.frame(maxWidth: .infinity, minHeight: 50, maxHeight: 50).background(Color(red: 146/255, green: 107/255, blue: 214/255, opacity: 1.0))
+                            .cornerRadius(10)
+                            .padding(.horizontal)
+                        }.frame(maxWidth: .infinity, maxHeight: .infinity).background(Color(.white))
+                    }
+                }
             }.edgesIgnoringSafeArea(.all)
         }.onAppear(perform: {
             self.FirstInitial = String(Fname.prefix(1))
